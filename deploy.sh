c2ca7cc49ba4373a9ef33ed6bdee643afba560b1
Brady Hurlburt <brady.hurlburt@rovercode.com>
2020-08-31 10:47:11 -0400

    Fix build dir path in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
+cp ./build/bbc-microbit-classic-gcc/source/rovercode-gigglebot-mbed-combined.hex ./rovercode.hex
 aws s3 sync \
     --metadata BuildNumber=${GITHUB_RUN_ID} \
     --acl public-read \
     --content-disposition attachment \
-    built/ s3://rovercode-gigglebot-mbed/$1/
+    build/ s3://rovercode-gigglebot-mbed/$1/
