f644e0f1228446ce00246bc158493f3dee060697
Brady Hurlburt <brady.hurlburt@rovercode.com>
2020-08-31 12:07:15 -0400

    Fix hex renaming path in deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-cp ./build/bbc-microbit-classic-gcc/source/rovercode-gigglebot-mbed-combined.hex ./rovercode.hex
+cp ./build/bbc-microbit-classic-gcc/source/rovercode-gigglebot-mbed-combined.hex ./build/rovercode.hex
 aws s3 sync \
     --metadata BuildNumber=${GITHUB_RUN_ID} \
     --acl public-read \
